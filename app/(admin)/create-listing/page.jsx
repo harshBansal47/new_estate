bf89ff1f30de6ba6476a603c3b2904b68026bb54
harshBansal47 <harshbansal472000@gmail.com>
2024-09-15 15:02:54 +0530

    commit

--- app/(admin)/create-listing/page.jsx
+++ app/(admin)/create-listing/page.jsx
@@ -2,15 +2,9 @@ import dynamic from "next/dynamic";
 import CreateListing from "@/components/dashboard/create-listing";
 
 export const metadata = {
-<<<<<<< HEAD
   title: 'Create Listing ||Prop Trade Real Estate',
   description:
     'Prop Trade Real Estate',
-=======
-  title: 'Create Listing || FindHouse - Real Estate React Template',
-  description:
-    'FindHouse - Real Estate React Template',
->>>>>>> master
 }
 
 const index = () => {
