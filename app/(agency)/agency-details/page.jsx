fc75bc3d84b86cb77ab4f03f18999245e74d3895
harshBansal47 <harshbansal472000@gmail.com>
2024-09-15 15:06:47 +0530

    commmit

--- app/(agency)/agency-details/page.jsx
+++ app/(agency)/agency-details/page.jsx
@@ -2,15 +2,9 @@ import dynamic from "next/dynamic";
 import AgencyDetails from "@/components/agency-details";
 
 export const metadata = {
-<<<<<<< HEAD
   title: 'Agency Details ||Prop Trade Real Estate',
   description:
     'Prop Trade Real Estate',
-=======
-  title: 'Agency Details || FindHouse - Real Estate React Template',
-  description:
-    'FindHouse - Real Estate React Template',
->>>>>>> master
 }
 
 const index = () => {
--- components/common/listing-details/PropertyFeatures.jsx
+++ components/common/listing-details/PropertyFeatures.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const PropertyFeatures = () => {
   const propertyFeatures = [
     {
@@ -22,24 +21,3 @@ const PropertyFeatures = () => {
 };
 
 export default PropertyFeatures;
-=======
-// const PropertyFeatures = () => {
-export const propertyFeatures = [
-  {
-    id: 1,
-    list: ["Air Conditioning", "Barbeque", "Dryer", "Gym", "Laundry"],
-  },
-  {
-    id: 2,
-    list: ["Lawn", "Microwave", "Outdoor Shower", "Refrigerator", "Sauna"],
-  },
-  {
-    id: 3,
-    list: ["Swimming Pool", "TV Cable", "Washer", "WiFi", "Window Coverings"],
-  },
-];
-// return <></>;
-//};
-
-// export default PropertyFeatures;
->>>>>>> master
